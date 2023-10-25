d1d2bbdbd4860e942c71be218c989552abd0662c
Han JiHo <h2j970313@gmail.com>
2023-10-25 23:25:11 +0900

    add: 컴포넌트 스캔 시작 위치 지정 어노테이션과 @SpringBootApplication의 내부 정보 및 컴포넌트 스캔 기본대상 학습(Controller, Service, Repository)

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        // 탐색할 패키지의 시작 위치를 지정한다.
+//         basePackages = "hello.core",
+        // 지정한 클래스의 패키지를 탐색 시작 위치로 지정한다.
+//         basePackageClasses = AutoAppConfig.class,
+        // 지정하지 않으면 @ComponentScan가 붙은 설정 정보 클래스의 패키지가 시작위치가 된다.
         // 제외할 컴포넌트 타입 설정하기
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -3,6 +3,7 @@ package hello.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// "@SpringBootApplication" 안에 "@ComponentScan"이 내포되어 있음
 @SpringBootApplication
 public class CoreApplication {
 
