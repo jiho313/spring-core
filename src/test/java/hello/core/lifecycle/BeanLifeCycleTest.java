704b01eae19869fd791b311487ff71251647581e
Han JiHo <h2j970313@gmail.com>
2023-11-27 23:33:16 +0900

    test

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -3,8 +3,8 @@ package hello.core.lifecycle;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 
 public class BeanLifeCycleTest {
 
@@ -22,7 +22,7 @@ public class BeanLifeCycleTest {
         @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
-            networkClient.setUrl("http://hello-spring.dev");
+//            networkClient.setUrl("http://hello-spring.dev");
             return networkClient;
         }
     }
