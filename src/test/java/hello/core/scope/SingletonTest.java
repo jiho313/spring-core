691db3e40c5dd9bf7970dc09d2557d48d64e3642
Han JiHo <h2j970313@gmail.com>
2023-12-02 19:47:44 +0900

    add: singletoneBean과 prototypeBean의 차이점
    싱글톤 빈은 스프링 컨테이너 생성 시점에서 생성, 의존관계 주입, 초기화가 이루어지고, 여러번 조회하더라도 단 하나의 빈 객체를 반환하며 컨테이너가 종료될 때 까지 관리되기 때문에 @PreDestroy 같은 빈 종료 메서드가 실행된다.
    프로토 타입 빈은 빈 조회 시점에서 생성, 의존관계 주입, 초기화가 실행되고 조회할 때마다 새로운 빈 객체를 생성해 반환한다. 그리고 스프링은 초기화까지만 관여하기 때문에 빈 종료 메서드가 실행되지 않는다.

---
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,44 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class SingletonTest {
+
+    @Test
+    void singletonBeanFind() {
+        // 컨테이너 생성 시점에서 초기화 메서드가 실행된다.
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+        System.out.println("find singletonBean1");
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        System.out.println("find singletonBean2");
+        // 둘은 같은 객체다.
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+        Assertions.assertThat(singletonBean1).isSameAs(singletonBean2);
+
+        // 싱글톤 빈은 스프링 컨테이너가 종료될 때까지 관리하므로 컨테이너 종료될 때 @PreDestroy와 같은 빈의 종료 메서드가 실행된다.
+        ac.close();
+    }
+
+    // 기본 값이 "singleton"이라 적지 않아도 된다.
+    @Scope("singleton")
+    static class SingletonBean {
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+    }
+}
