fe0ac4e817891ecc6b3fbfbbc20f4c1418a43431
Han JiHo <h2j970313@gmail.com>
2024-02-08 23:59:07 +0900

    add: singletoneBean과 prototypeBean 함께 사용하기 위해 Provider 할용
    1. 스프링이 제공하는 ObjectFactory, ObjectProvider 사용 -  ObjectProvider는 ObjectFactory 상속, 옵션, 스트림 처리등 편의 기능이 많고, 둘 다 별도의 라이브러리 필요 없고, 스프링에 의존한다.
    2. 자바 표준이 제공하는 JSR-330 Provider 사용 - get()으로 기능이 매우 단순하고, 별도의 라이브러리가 필요하고, 자바 표준이므로 스프링이 아닌 다른 컨테이너에서도 사용할 수 있다.

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,11 +1,14 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,19 +29,18 @@ public class SingletonWithPrototypeTest1 {
 
     @Test
     void singletonClientUsePrototype() {
-        AnnotationConfigApplicationContext ac =
-                new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
 
         ClientBean clientBean1 = ac.getBean(ClientBean.class);
         int count1 = clientBean1.logic();
         assertThat(count1).isEqualTo(1);
 
-        ClientBean clientBean2= ac.getBean(ClientBean.class);
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
         /*의도한 것은 각 클라이언트 빈이 의존하는 프로토타입 빈을 사용할 때마다 새로 만들어 사용하려는 것이다.
-        * 하지만 싱글톤은 빈 생성시점에만 의존관계를 주입받기 때문에, 프로토타입 빈이 새로 생성되긴 하지만,
-        * 싱글톤 빈과 함께 계속 유지되기 때문에 의도한대로 작동하지 않는다.*/
+         * 하지만 싱글톤은 빈 생성시점에만 의존관계를 주입받기 때문에, 프로토타입 빈이 새로 생성되긴 하지만,
+         * 싱글톤 빈과 함께 계속 유지되기 때문에 의도한대로 작동하지 않는다.*/
 
     }
 
@@ -46,13 +48,23 @@ public class SingletonWithPrototypeTest1 {
     static class ClientBean {
         // 내부에 가지고 있는 프로토타입 빈은 이미 과거에 생성 시점에 주입이 끝난 빈이다.
         // 주입 시점에 스프링 컨테이너에 요청해서 프로토타입 빈이 새로 생성이 된 것이지, 사용 할 때마다 새로 생성되는 것이 아니다.
-        private final PrototypeBean prototypeBean; // 생성 시점에 주입
+//        private final PrototypeBean prototypeBean; // 생성 시점에 주입
 
-        ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+//        @Autowired
+//        ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
+
+        /*ObjectProvider는 지정한 빈을 컨테이너에서 찾아 DL서비스를 제공해준다.
+         * ObjectFactory 가 있었는데, 여기에 편의 기능을 추가해서 ObjectProvider 가 만들어졌다
+         * 의도한대로 필요할 때마다 스프링 컨테이너에 요청을 해서 새로 생성 후 사용한다.*/
+        @Autowired
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
